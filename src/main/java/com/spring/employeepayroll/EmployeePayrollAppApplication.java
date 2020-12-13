55bd05f3012140f41b0a075684684795a5a18f21
Ard2238 <abhishekrdas28@gmail.com>
2020-12-14 04:50:17 +0530

    Refactored Code and application.properties file to enable logging using lombok library and slf4j framework

--- src/main/java/com/spring/employeepayroll/EmployeePayrollAppApplication.java
+++ src/main/java/com/spring/employeepayroll/EmployeePayrollAppApplication.java
@@ -1,13 +1,19 @@
 package com.spring.employeepayroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Payroll App");
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll Application started in {} environment", context.getEnvironment().getProperty("environment"));
     }
-
 }
+
