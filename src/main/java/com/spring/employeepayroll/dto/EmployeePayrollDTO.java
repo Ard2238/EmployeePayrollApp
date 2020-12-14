5c421cadddc6e11c6f0ee72649dd5784d27695c8
Ard2238 <abhishekrdas28@gmail.com>
2020-12-14 06:20:42 +0530

    Added Code to provide validations for attributes

--- src/main/java/com/spring/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/spring/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,7 +1,14 @@
 package com.spring.employeepayroll.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Invalid Employee Name")
     public String name;
+
+    @Min(value = 5000, message = "Minimum salary should be more than 5000")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary){
